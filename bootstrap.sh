1bc87c8d3887211200c59ec9463183d70b549b3e
Bram de Vries <bram.devries@nedap.com>
2019-02-20 22:17:24 +0100

    Fix mas-cli download url

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ set -e
 # mas-cli recommends using brew, but the ansible playbook will install brew for us
 if [ ! -f /usr/local/bin/mas ]; then
     echo "mas-cli was not found! Looking up latest version from Github releases"
-    mas_latest_release=$(curl -s https://api.github.com/repos/mas-cli/mas/releases/latest | python -c "import sys, json; sys.stdout.write(json.load(sys.stdin)['assets'][0]['browser_download_url']);");
+    mas_latest_release=$(curl -s https://api.github.com/repos/mas-cli/mas/releases/latest | python -c "import sys, json; sys.stdout.write(json.load(sys.stdin)['assets'][1]['browser_download_url']);");
     echo "Downloading and installing mas-cli from Github"
     curl -sL $mas_latest_release -o mas.zip > /dev/null
     sudo unzip mas.zip -d /usr/local/bin/ > /dev/null

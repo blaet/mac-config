93c3638c92721360d335c781ee994d2ebb80b001
Bram de Vries <bram.devries@nedap.com>
2019-02-21 11:29:25 +0100

    Change indentation

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,12 +6,12 @@ set -e
 # Check if the mas-cli exists and look up the latest release to install
 # mas-cli recommends using brew, but the ansible playbook will install brew for us
 if [ ! -f /usr/local/bin/mas ]; then
-    echo "mas-cli was not found! Looking up latest version from Github releases"
-    mas_latest_release=$(curl -s https://api.github.com/repos/mas-cli/mas/releases/latest | python -c "import sys, json; sys.stdout.write(json.load(sys.stdin)['assets'][1]['browser_download_url']);");
-    echo "Downloading and installing mas-cli from Github"
-    curl -sL $mas_latest_release -o mas.zip > /dev/null
-    sudo unzip mas.zip -d /usr/local/bin/ > /dev/null
-    rm mas.zip
+  echo "mas-cli was not found! Looking up latest version from Github releases"
+  mas_latest_release=$(curl -s https://api.github.com/repos/mas-cli/mas/releases/latest | python -c "import sys, json; sys.stdout.write(json.load(sys.stdin)['assets'][1]['browser_download_url']);");
+  echo "Downloading and installing mas-cli from Github"
+  curl -sL $mas_latest_release -o mas.zip > /dev/null
+  sudo unzip mas.zip -d /usr/local/bin/ > /dev/null
+  rm mas.zip
 fi
 
 echo "Bootstrapping complete"
